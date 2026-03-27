333c92d3b6ef08ece2c0fc73a0c1d30b46028159
XCTF12 <jat14@alfred.edu>
2026-03-27 10:25:29 -0400

    start new branch

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 def func1():
     lst = []
     print('Enter numbers one at a time. Enter 0 to stop.')
-    x = int(input('num:'))
-    while x != 0:
-        lst.append(x)
-        x = int(input('num:'))
+    inpNum = int(input('num:'))
+    while inpNum != 0:
+        lst.append(inpNum)
+        inpNum = int(input('num:'))
     return lst
 
 def func2(data):
@@ -14,15 +14,18 @@ def func2(data):
     choice = ''
     while True:
         choice = input('Select an option. 1-sum  2-min  3-max  4-quit')
-        if choice == '1':
-            print('The sum is',sum(data))
-        elif choice == '2':
-            print('Not supported yet')
-        elif choice == '3':
-            print('Not supported yet')
-        elif choice == '4':
-            print('Goodbye')
-            return
+        return new_func(data, choice)
+
+def new_func(data, choice):
+    if choice == '1':
+        print('The sum is',sum(data))
+    elif choice == '2':
+        print('Not supported yet')
+    elif choice == '3':
+        print('Not supported yet')
+    elif choice == '4':
+        print('Goodbye')
+        return
 
 def main():
     print("Hello, World!")
