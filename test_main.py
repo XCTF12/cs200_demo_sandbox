ae88fffc75202a12fb03b624e2b5bd66c3ef5b63
XCTF12 <147000498+XCTF12@users.noreply.github.com>
2026-04-06 11:01:51 -0400

    More testing added

--- test_main.py
+++ test_main.py
@@ -1,4 +1,19 @@
 import main
+import pytest
 
+@pytest.mark.parameterize(
+        ('input_x', 'input_y', 'expected'),
+        (0, 0, 1),
+        (1, 0, 1),
+        (1, 1, 2),
+        (2, 3, 5),
+        (1, 7, 8)
+
+)
 def test_main():
     assert main.sum(3,6) == 9, 'noooooo'
+
+def test_main2(input_x, input_y, expected):
+    assert main.sum(input_x, input_y) == expected
+
+test_main2()
