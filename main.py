ae15323fb93e57fe7a2cbaa7e4c238f6f3b3434a
XCTF12 <147000498+XCTF12@users.noreply.github.com>
2026-04-14 11:10:06 -0400

    Add input to main.py + add Dockerfile

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 def sum(n1,n2):
     return n1+n2
 
+def get_input():
+    n1 = input('Enter the first number:')
+    n2 = input('Enter the second number:')
+
+    return n1,n2
+    
+
 def main():
     print("Hello, World!")
-    s = sum(3,5)
-    print(s)
+    n1, n2 = get_input()
+    s = sum(n1,n2)
+    print(f'The sum is {s}')
 
 if __name__ == "__main__":
     main()
